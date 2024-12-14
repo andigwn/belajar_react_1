63f475bf014c5b18eac6e2597f2f37e7ab6d5437
andi gunawan <ag0227442@gmail.com>
2024-12-14 14:12:00 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 // parent component
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Testing from "./components/testing";
 
 const App = () => {
+  const [getLimit, setLimit] = useState(0);
+  const [getMyName, setMyName] = useState('Andi');
+
+  useEffect(() => {
+    console.log('render ketika browser di buka');
+  }, [])
   return (
     <>
+      <center>
+        {getLimit} <br />
+        <button onClick={() => setLimit((prev) => prev + 1)}>tambah</button>
+        <br />
+        {getLimit > 0 && <button onClick={() => setLimit(0)}>reset</button>}
+        {getLimit > 0 && <button onClick={() => setLimit((prev) => prev - 1)}>kurang</button>}
+      </center>
       <Testing />
     </>
   )
